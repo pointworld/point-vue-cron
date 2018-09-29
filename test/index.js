a7e2f2655d3dc55e519592f2d5701f123bcddee1
one <onerf@sina.com>
2018-09-29 13:16:30 +0800

    build

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import cron from '../src'
-import 'element-ui/lib/theme-default/index.css'
+// import 'element-ui/lib/theme-default/index.css'
 import Test from '../test/test.vue'
 
 Vue.use(cron)
